bbd09ab3154f6e54bcaaf2a7b962ab419d67a979
Thanisorn <thanisorn.sir@gmail.com>
2025-07-21 14:05:50 +0700

    feat: solve ex-1 to ex-6

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,11 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
+
 // Start coding here
+for (const key in companyName) {
+    console.log(`Number ${Number(key)+1}  character is ${companyName[key]}`);
+}
+
+
+
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,10 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+console.log(companyName.length);
+for (let i=companyName.length-1; i>=0; i-=1) {
+    reversedCompanyName = reversedCompanyName + companyName[i];
+}
 
 console.log(reversedCompanyName);
+
